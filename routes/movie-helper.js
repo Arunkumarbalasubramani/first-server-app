a86d644c71779a02210b8c24121d85907c7a16b4
Arunkumarbalasubramani <arunkumar.vbs.b@gmail.com>
2022-09-26 06:36:09 +0530

    Changed ID to Object_id

--- routes/movie-helper.js
+++ routes/movie-helper.js
@@ -1,22 +1,28 @@
 import { client } from "../index.js";
-
+import { ObjectId } from "mongodb";
 export async function addMovie(newmovies) {
   return await client.db("b33we").collection("movies").insertMany(newmovies);
 }
 
 export async function deleteMovies(id) {
-  return await client.db("b33we").collection("movies").deleteOne({ id: id });
+  return await client
+    .db("b33we")
+    .collection("movies")
+    .deleteOne({ _id: ObjectId(id) });
 }
 
 export async function updateMovies(id, update) {
   return await client
     .db("b33we")
     .collection("movies")
-    .updateOne({ id: id }, { $set: update });
+    .updateOne({ _id: ObjectId(id) }, { $set: update });
 }
 
 export async function getMoviesById(id) {
-  return await client.db("b33we").collection("movies").findOne({ id: id });
+  return await client
+    .db("b33we")
+    .collection("movies")
+    .findOne({ _id: ObjectId(id) });
 }
 
 export async function getAllMovies(request) {
